181840a979ef9cb4b384235d649b56b590cebbdf
Tu Nguyen <im2nguyen@gmail.com>
2021-05-28 11:19:27 -0700

    Add quotes to variable/output names

--- variables.tf
+++ variables.tf
@@ -1,42 +1,42 @@
-# Variable declarations
+# variable "declarations
 
-variable aws_region {
+variable "aws_region" {
   description = "AWS region"
   type        = string
   default     = "us-west-2"
 }
 
-variable vpc_cidr_block {
+variable "vpc_cidr_block" {
   description = "CIDR block for VPC"
   type        = string
   default     = "10.0.0.0/16"
 }
 
-variable instance_count {
+variable "instance_count" {
   description = "Number of instances to provision."
   type        = number
   default     = 2
 }
 
-variable enable_vpn_gateway {
+variable "enable_vpn_gateway" {
   description = "Enable a VPN gateway in your VPC."
   type        = bool
   default     = false
 }
 
-variable public_subnet_count {
+variable "public_subnet_count" {
   description = "Number of public subnets."
   type        = number
   default     = 2
 }
 
-variable private_subnet_count {
+variable "private_subnet_count" {
   description = "Number of private subnets."
   type        = number
   default     = 2
 }
 
-variable public_subnet_cidr_blocks {
+variable "public_subnet_cidr_blocks" {
   description = "Available cidr blocks for public subnets"
   type        = list(string)
   default     = [
@@ -51,7 +51,7 @@ variable public_subnet_cidr_blocks {
   ]
 }
 
-variable private_subnet_cidr_blocks {
+variable "private_subnet_cidr_blocks" {
   description = "Available cidr blocks for private subnets"
   type        = list(string)
   default     = [
@@ -66,7 +66,7 @@ variable private_subnet_cidr_blocks {
   ]
 }
 
-variable resource_tags {
+variable "resource_tags" {
   description = "Tags to set for all resources"
   type        = map(string)
   default     = {
@@ -75,7 +75,7 @@ variable resource_tags {
   }
 }
 
-variable ec2_instance_type {
+variable "ec2_instance_type" {
   description = "AWS EC2 instance type."
   type        = string
   default     = "t2.micro"
